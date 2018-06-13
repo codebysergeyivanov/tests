9593a10e3a56d49825bd541d79f25aa6ebd46930
be2much4ne1 <79231530812@yandex.ru>
2018-06-13 08:25:15 +0700

    1 test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,7 +5,7 @@ suite('Unit Tests', function(){
   
   // Make ALL tests pass
   // !! Don't scramble the Assertions. We rely on their order to check the results !!
-  suite('Basic Assertions', function() {
+  suite('Basic As1tesertions', function() {
     /** assert.fail() will always fail. Change it into something more useful... **/
   
     /** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
